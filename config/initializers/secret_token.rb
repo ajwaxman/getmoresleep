42f5c426ec3c33051df7f4f919c1e5d5741657e0
Adam Waxman <awaxman11@gmail.com>
2013-08-09 18:56:31 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Getmoresleep::Application.config.secret_token = '554946b7f2215e52cb6fedde024b1ec019b41d70c565333d4ce2534e2c689b1a811216788f0fc263f7c6ba67cb9c9ded0cf7a5abb106241e5745747585c5a39f'
