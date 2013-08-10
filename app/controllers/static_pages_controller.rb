360a59d4d6f818a5535254bb70c6dbfeb77b7043
Adam Waxman <awaxman11@gmail.com>
2013-08-10 12:44:01 -0400

    Get auth working

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,11 +5,10 @@ class StaticPagesController < ApplicationController
 
   def auth
     code = params["code"]
-    json = HTTParty.post("https://jawbone.com/auth/oauth2/token", body: ', grant_type: 'authorization_code', code: code }).body
+    json = HTTParty.post("https://jawbone.com/auth/oauth2/token", body: {client_id: ENV["JAWBONE_CLIENT_ID"], client_secret: ENV["JAWBONE_CLIENT_SECRET"], grant_type: 'authorization_code', code: code }).body
     result = JSON.parse(json)
     access_token = "Bearer " + result["access_token"]
-
-    @user = HTTParty.get("https://jawbone.com/nudge/api/users/@me", :query => {}, :headers => { "Authorization: " => access_token })
+    @user = HTTParty.get("https://jawbone.com/nudge/api/users/@me", :headers => { "Authorization" => access_token })
     raise params
     # redirect_to root_path
   end
