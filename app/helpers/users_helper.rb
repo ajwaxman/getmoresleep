68f0fcc7c0a9f23653b62cc41d334ad7bfef2c21
dmanaster <david@manaster.com>
2013-08-11 14:53:23 -0400

    Add some methods for determining times last night.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,15 +1,36 @@
 module UsersHelper
 
-  def asleep_hours(asleep_length)
+  def get_hours(asleep_length)
     asleep_length/60/60
   end
 
-  def asleep_minutes(asleep_length)
+  def get_minutes(asleep_length)
     asleep_length/60%60
   end
 
-  def format_minutes(time)
-    time.to_s.gsub!(/\A(\d)\z/, '0\1')
+  def add_leading_zero(time)
+    time.to_s.gsub(/\A(\d)\z/, '0\1')
   end
+
+  def time_now
+    Time.now.to_i
+  end
+
+  def did_user_sleep_last_night?
+    (time_now - @sleep_time) < (24*60*60)
+  end
+
+  def trigger_time(num)
+    case num
+    when 9, 10, 11
+      (Time.now.beginning_of_day - (60*60*(12-num))).to_i
+    when 12
+      (Time.now.beginning_of_day).to_i
+    when 1, 2, 3, 4, 5, 6, 7
+      (Time.now.beginning_of_day + (60*60*num)).to_i
+    end
+  end
+
+
 
 end
